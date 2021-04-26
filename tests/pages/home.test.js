8b2b30093e13f8964a330ae161b7060f258070ad
ElDav1d <dvivoes@yahoo.es>
2021-04-26 11:36:30 +0200

    FIX home tests

--- tests/pages/home.test.js
+++ tests/pages/home.test.js
@@ -1,9 +1,22 @@
 import { render, screen } from '../test-utils.js';
+
+import { VisitedCharactersContextProvider } from '../../context/visitedCharacters';
 import Home from '../../pages/index.tsx';
 
 describe('Home', () => {
-  it('renders home page', () => {
-    render(<Home />)
-    expect(screen.getByText(/hello world/i));
+  it('renders home page with one header, main and footer tags ', () => {
+    render(
+      <VisitedCharactersContextProvider>
+        <Home />
+      </VisitedCharactersContextProvider>
+    );
+    const header = screen.getByRole('banner',);
+    const main = screen.getByRole('main');
+    const footer = screen.getByRole('contentinfo');
+
+
+    expect(header).toBeInTheDocument();
+    expect(main).toBeInTheDocument();
+    expect(footer).toBeInTheDocument();
   })
 })
