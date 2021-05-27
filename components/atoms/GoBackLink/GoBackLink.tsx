9ad278c39259e31b177d40f60559ca0be70021e2
ElDav1d <dvivoes@yahoo.es>
2021-05-27 18:26:21 +0200

    IMprove go back link component logic

--- components/atoms/GoBackLink/GoBackLink.tsx
+++ components/atoms/GoBackLink/GoBackLink.tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import styled from "styled-components";
 
 const StyledGoBackLink = styled.a`
+  text-decoration: none;
   position: relative;
   display: inline-block;
   color: blue;
@@ -22,8 +23,49 @@ const StyledGoBackLink = styled.a`
 
 const GoBackLink = () => {
   const router = useRouter();
+  const currentQuery = router.query.id.toString();
+  const listerPath = "characters-list";
+
+  const getListerPageQuery = (queryID: string) => {
+    const characterID = parseInt(queryID);
+    let listerPageQuery: number;
+    switch (true) {
+      case characterID < 11:
+        listerPageQuery = 1;
+        break;
+      case characterID < 21:
+        listerPageQuery = 2;
+        break;
+      case characterID < 31:
+        listerPageQuery = 3;
+        break;
+      case characterID < 41:
+        listerPageQuery = 4;
+        break;
+      case characterID < 51:
+        listerPageQuery = 5;
+        break;
+      case characterID < 61:
+        listerPageQuery = 6;
+        break;
+      case characterID < 71:
+        listerPageQuery = 7;
+        break;
+      case characterID < 81:
+        listerPageQuery = 8;
+        break;
+      default:
+        listerPageQuery = 9;
+    }
+    return listerPageQuery;
+  };
+
   return (
-    <StyledGoBackLink onClick={() => router.back()}>GO BACK</StyledGoBackLink>
+    <StyledGoBackLink
+      href={`/${listerPath}/${getListerPageQuery(currentQuery)}`}
+    >
+      GO BACK
+    </StyledGoBackLink>
   );
 };
 
